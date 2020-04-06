3e7654e5597a88851b7a6be6490cc926b3068be4
Nico-L <luchier@gmail.com>
2020-04-06 15:19:42 +0000

    correct env var

--- resolvers/clSignature.js
+++ resolvers/clSignature.js
@@ -1,17 +1,12 @@
 var crypto = require("crypto");
-const api_secret = process.env.CLOUDINARY_API_KEY;
+const api_secret = process.env.CLOUDINARY_KEY;
 
 const resolvers = {
   Query: {
     clSignature: async (parent, args) => {
-      console.log("args", args);
       var shasum = crypto.createHash("sha1");
       shasum.update(args.to_sign + api_secret, "binary");
       const retour = shasum.digest("hex");
-      /*let retour = await cloudinary.v2.uploader.destroy(
-        args.toSign,
-        api_secret
-      ); */
       return {
         signature: retour
       };
