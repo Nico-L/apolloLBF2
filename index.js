5bbacf7b680e786afb308f073f7b9dcccdd1d862
Nico-L <luchier@gmail.com>
2020-04-23 12:55:42 +0000

    ajuste netlify hook

--- index.js
+++ index.js
@@ -3,11 +3,30 @@ const GraphQLHelper = require("./helpers/graphql");
 const fetch = require('node-fetch');
 const CronJob = require('cron').CronJob;
 
+const body= {query: `mutation setTestBuild {
+            update_buildNeeded(where: {espace: {_eq: "atelier"}}, _set: {buildNeeded: false}) {
+                affected_rows
+            }
+            }
+            `}
+
+var options = {
+        method: "POST",
+        headers: {},
+        cache: "no-cache",
+        body: JSON.stringify(body)
+    }
+
 const job = new CronJob('00 00 00 * * *', function() {
     fetch(process.env.HOOK_ATELIER, {
         method: 'POST',
         headers: {},
         body: ''
+      }).then((retour)=> {
+          fetch(process.env.HASURA_URL, options)
+        .then(async retour => {
+            console.log('retour', retour)
+        }).catch((error)=>{console.log('erreur va chercher', error)})
       })
 });
 
