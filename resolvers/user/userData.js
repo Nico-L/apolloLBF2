48edee7575b7cc64a94cd56cc06b5a8114f3245e
Nico-L <luchier@gmail.com>
2020-07-17 09:04:13 +0000

    modif info user

---
+++ resolvers/user/userData.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const tokenSite = process.env.TOKEN_SITE
+const cms = process.env.ADRESSE_CMS
+
+const resolvers = {
+    Query: {
+        userData: async (parent, args) => {
+            const headers = {
+                'Content-Type': 'application/json'
+            }
+            const adresse = cms +'users?email=' + args.email + '&token=' + tokenSite
+            user = await axios.get(adresse, headers)
+                .then((retour) => {return retour.data})
+                .catch((error) => console.log('erreur userData', error))
+            let userData = {}
+            if (user[0]) {
+                let u = user[0]
+                userData.id = u.id
+                userData.abonnement = u.abonnementMachine
+                userData.cnc = u.cnc
+                userData.laser = u.laser
+                userData.imprimante3D = u.imprimante3D
+                userData.scie_toupie = u.scie_toupie
+                userData.rabo_degau = u.rabo_degau
+            }
+            return {
+                userData: JSON.stringify(userData)
+            }
+        }
+    }
+}
+
+module.exports = resolvers;
