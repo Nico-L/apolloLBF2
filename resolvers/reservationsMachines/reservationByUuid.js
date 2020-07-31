a3a3b1d3c8a4243488502fe2c24915c90925a3c1
Nico-L <luchier@gmail.com>
2020-07-31 07:16:37 +0000

    recup resa par uuid

---
+++ resolvers/reservationsMachines/reservationByUuid.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const tokenSite = process.env.TOKEN_SITE
+const cms = process.env.ADRESSE_CMS
+
+const resolvers = {
+    Query: {
+        reservationByUuid: async (parent, args) => {
+            const headers = {
+                'Content-Type': 'application/json'
+            }
+            const adresse = cms + 'reservations-machines?uuid=' + args.uuid + '&token=' + tokenSite
+            reservationByUuid = await axios(adresse, headers)
+                .then((retour) => {return retour.data})
+                .catch((erreur) => {console.log('erreur reservationByUuid', erreur)})
+            return {
+                reservationByUuid: JSON.stringify(reservationByUuid)
+            }
+        }
+    }
+}
+
+module.exports = resolvers;
